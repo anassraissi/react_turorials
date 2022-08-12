146dd06d5b68b8949d7d48228b8b7ee359c47562
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2022-08-12 10:58:37 +0100

    set css dynamicly by js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+      let classes='';
+      if(true){
+          classes+='color ';
+      }
+      if(true){
+        classes+='text_bold ';
+      }
+      if(true){
+        classes+='background_color ';
+      }
+      classes=[''];
+      if(true){
+        classes.push('color');
+      }
+      if(true){
+        classes.push('text_bold');
+      }
+      if(true){
+        classes.push('background_color');
+      }
+
+       // classes.join(' ') zid espace mabib les classs  katwali color text_bold background_color
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.join(' ')}>  
+        Test
+      </div>
     </div>
   );
 }
